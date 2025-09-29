24657020d4343a45692179de65d2be8d321364d7
kbb306 <kylerbbelshaw@gmail.com>
2025-09-29 12:53:20 -0600

    Created FantCal.py

---
+++ FantCal.py
@@ -0,0 +1,5 @@
+yearval, monthval,weekval = 0
+while ((yearval <= 0 ) or (monthval <= 0) or (weekval <= 0)):
+    weekval= input("How many days in a week? ")
+    monthval = input("How many weeks in a month? ")
+    yearval = input("How many months in a year? ")
